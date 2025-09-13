afe65d7c9c58829de1f59d44c8583b44e0ad275b
Alejandro Leal <aleaberm12@gmail.com>
2025-09-13 14:49:03 +0200

    colors, remove old files

--- updater.sh
+++ updater.sh
@@ -6,18 +6,18 @@ COLOR_YELLOW='\033[0;33m'
 COLOR_BLUE='\033[0;34m'
 COLOR_DEFAULT='\033[0m'
 
-read -p "Target version: " COMFYUI_VERSION
-read -p "Current ComfyUI path (example: C:/Users/user/Documents/ComfyUI_windows_portable): " COMFYUI_PATH
+read -p "Target version (example: 0.3.59): " COMFYUI_VERSION
+read -p "Current ComfyUI path (example: C:/Users/your_user/Documents/ComfyUI_windows_portable): " COMFYUI_PATH
 
 # Validate version (not empty or only spaces)
 if [[ -z "${COMFYUI_VERSION// }" ]]; then
-	echo "Error: You must specify a target version."
+	echo -e "${COLOR_RED}Error: You must specify a target version.${COLOR_DEFAULT}"
 	exit
 fi
 
 # Validate version format (X.X.X)
 if ! [[ "$COMFYUI_VERSION" =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
-	echo "Error: Target version must be in format X.X.X (e.g. 1.2.3)."
+	echo -e "${COLOR_RED}Error: Target version must be in format X.X.X (e.g. 0.3.59).${COLOR_DEFAULT}"
 	exit
 fi
 
@@ -25,7 +25,7 @@ DOWNLOAD_URL="https://github.com/comfyanonymous/ComfyUI/releases/download/v$COMF
 
 # Validate path (not empty or only spaces)
 if [[ -z "${COMFYUI_PATH// }" ]]; then
-	echo "Error: ComfyUI path cannot be empty or only spaces."
+	echo -e "${COLOR_RED}Error: ComfyUI path cannot be empty or only spaces.${COLOR_DEFAULT}"
 	exit
 fi
 
@@ -34,7 +34,7 @@ DIRS=("ComfyUI" "python_embeded" "update")
 
 for dir in "${DIRS[@]}"; do
 	if [ ! -d "$COMFYUI_PATH/$dir" ]; then
-		echo "Error: Directory '$dir' not found in '$COMFYUI_PATH'."
+		echo -e "${COLOR_RED}Error: Directory '$dir' not found in '$COMFYUI_PATH'.${COLOR_DEFAULT}"
 		exit
 	fi
 done
@@ -44,23 +44,25 @@ FILES=("run_cpu.bat" "run_nvidia_gpu.bat")
 
 for file in "${FILES[@]}"; do
 	if [ ! -f "$COMFYUI_PATH/$file" ]; then
-		echo "Error: File '$file' not found in $COMFYUI_PATH."
+		echo -e "${COLOR_RED}Error: File '$file' not found in $COMFYUI_PATH.${COLOR_DEFAULT}"
 		exit
 	fi
 done
 
 # Create 'updating' directory
 UPDATING_PATH="$COMFYUI_PATH/updating"
+COMFY_UPDATING_PATH="$UPDATING_PATH/ComfyUI_windows_portable"
+
 rm -rf "$UPDATING_PATH"
 mkdir -p "$UPDATING_PATH"
 
 # Download the file to the 'updating' directory
 DOWNLOAD_FILE="$UPDATING_PATH/ComfyUI_windows_portable_nvidia.7z"
-echo "Downloading to $DOWNLOAD_FILE..."
+echo -e "${COLOR_BLUE}Downloading to $DOWNLOAD_FILE...${COLOR_YELLOW}"
 curl -L "$DOWNLOAD_URL" -o "$DOWNLOAD_FILE"
 
 # Extract the file to the 'updating' directory
-echo "Extracting..."
+echo -e "${COLOR_BLUE}Extracting...${COLOR_YELLOW}"
 7z x "$DOWNLOAD_FILE" -o"$UPDATING_PATH"
 
 # Move directories from ../ComfyUI/models to ../updating/ComfyUI_windows_portable/ComfyUI/models
@@ -68,20 +70,41 @@ MODELS_DIRS=("checkpoints" "controlnet" "inpaint" "loras" "upscale_models")
 
 for model_dir in "${MODELS_DIRS[@]}"; do
 	if [ -d "$COMFYUI_PATH/ComfyUI/models/$model_dir" ]; then
-		echo "Preparing to move $model_dir..."
- 		rm -rf "$UPDATING_PATH/ComfyUI_windows_portable/ComfyUI/models/$model_dir"
+		echo -e "${COLOR_YELLOW}Preparing to move $model_dir...${COLOR_DEFAULT}"
+ 		rm -rf "$COMFY_UPDATING_PATH/ComfyUI/models/$model_dir"
 
-		echo "Moving $model_dir..."
- 		mv "$COMFYUI_PATH/ComfyUI/models/$model_dir" "$UPDATING_PATH/ComfyUI_windows_portable/ComfyUI/models/"
+		echo -e "${COLOR_YELLOW}Moving $model_dir...${COLOR_DEFAULT}"
+ 		mv "$COMFYUI_PATH/ComfyUI/models/$model_dir" "$COMFY_UPDATING_PATH/ComfyUI/models/"
 	fi
 done
 
 # Move content from ../ComfyUI/output to ../updating/ComfyUI_windows_portable/ComfyUI/output
 if [ -d "$COMFYUI_PATH/ComfyUI/output" ]; then
-	echo "Moving output..."
- 	mv "$COMFYUI_PATH/ComfyUI/output/"* "$UPDATING_PATH/ComfyUI_windows_portable/ComfyUI/output/"
+	echo -e "${COLOR_BLUE}Moving output...${COLOR_DEFAULT}"
+ 	mv "$COMFYUI_PATH/ComfyUI/output/"* "$COMFY_UPDATING_PATH/ComfyUI/output/"
 fi
 
 # Setup ComfyUI-Manager
-echo "Cloning ComfyUI-Manager into custom_nodes..."
-git clone https://github.com/ltdrdata/ComfyUI-Manager "$UPDATING_PATH/ComfyUI_windows_portable/ComfyUI/custom_nodes/comfyui-manager"
+echo -e "${COLOR_BLUE}Cloning ComfyUI-Manager into custom_nodes...${COLOR_YELLOW}"
+git clone https://github.com/ltdrdata/ComfyUI-Manager "$COMFY_UPDATING_PATH/ComfyUI/custom_nodes/comfyui-manager"
+
+# Remove old files
+echo -e "${COLOR_BLUE}Removing old files...${COLOR_DEFAULT}"
+
+for item in "$COMFY_UPDATING_PATH"/*; do
+	name=$(basename "$item")
+	target="$COMFYUI_PATH/$name"
+
+	if [ -e "$target" ]; then
+		echo -e "${COLOR_YELLOW}Removing $target...${COLOR_DEFAULT}"
+		rm -rf "$target"
+	fi
+
+	echo -e "${COLOR_BLUE}Moving $item to $COMFYUI_PATH/...${COLOR_DEFAULT}"
+	mv "$item" "$COMFYUI_PATH/"
+done
+
+echo -e "${COLOR_BLUE}Removing $UPDATING_PATH...${COLOR_DEFAULT}"
+rm -rf "$UPDATING_PATH"
+
+echo -e "${COLOR_GREEN}Update to version $COMFYUI_VERSION completed successfully!${COLOR_DEFAULT}"
